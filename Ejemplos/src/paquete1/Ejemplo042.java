a8ec3db5f9cd357115fd742442f1c235a47382ed
Bananin-DEV <mjvaldivieso6@utpl.edu.ec>
2025-12-10 12:23:20 -0500

    Segundo commit

--- Ejemplos/src/paquete1/Ejemplo042.java
+++ Ejemplos/src/paquete1/Ejemplo042.java
@@ -9,27 +9,35 @@ package paquete1;
  *
  * @author reroes
  */
-public class Ejemplo04 {
+public class Ejemplo042 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // Declarar y generar arreglos en Java
-        
+
         // arreglo de tipo entero
         // int[] arreglo1 = new int[5];
+        int suma = 0;
         int[] arreglo1 = {1000, 1, 2, 2000, 300};
+        String acumulador = "";
         // System.out.println(arreglo1.length);
         // arreglo1[2] = 1000;
-        // el .lenght recorre cuantos valores tiene el arreglo, en este caso como es
-        // de 5 valores el condicional va a funcionar hasta que i<5
         for (int i = 0; i < arreglo1.length; i++) {
-            System.out.printf("Subíndice o Índice %d, con valor %d\n",
-                    i, arreglo1[i]);
+            if (arreglo1[i] >= 1000) {
+
+                suma = suma + arreglo1[i];
+                acumulador = String.format("%sSubíndice o Índice %d, "
+                        + "con valor %d\n",
+                        acumulador, i, arreglo1[i]);
+            }
         }
-        
-        
+        acumulador = String.format("%sLa suma total es: %d\n",
+                acumulador, suma);
+        System.out.printf("%s\n",
+                acumulador);
+
     }
 
 }
